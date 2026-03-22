b19d75e65c250c0ac75cf885205aa4b4eeaf3885
Rubel Rudra <rubelrudra27@gmail.com>
2026-03-22 10:24:09 +0600

    create a register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,19 @@
+import RegisterPage from "@/components/modules/auth/register/RegisterForm";
+import { Suspense } from "react";
+import { Loader2 } from "lucide-react";
+
+const SignUpPage = () => {
+  return (
+    <div className="container mx-auto px-4">
+      <Suspense fallback={
+        <div className="min-h-screen flex items-center justify-center bg-[#0a0a14]">
+          <Loader2 className="animate-spin text-orange-500" size={48} />
+        </div>
+      }>
+        <RegisterPage />
+      </Suspense>
+    </div>
+  );
+};
+
+export default SignUpPage;
